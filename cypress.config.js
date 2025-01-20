c1890dd878cf73c86152b3d63ff6ce464472346c
Gold-Hunter <redgoldenman@gmail.com>
2025-01-19 21:07:45 -0500

    pls

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: true,  
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',                    
+    retries: {
+      runMode: 2,                     
+      openMode: 0                     
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config);
+      return config;
     },
   },
 });
